1f8b12500e32dfcab4a560ef014bf4eb21bfa5b3
Peter Willumsen <pewillumsen@gmail.com>
2021-12-02 11:11:55 +0100

    .

--- day02/stateMachine.py
+++ day02/stateMachine.py
@@ -13,13 +13,13 @@ class StateMachine:
 
         
             if instruction.startswith("forward "):
-                self.memory["forward"] = self.memory["forward"] + int(instruction[8])
+                self.memory["forward"] += int(instruction[8])
 
             if instruction.startswith("down "):
-                self.memory["down"] = self.memory["down"] + int(instruction[5])
+                self.memory["down"] += int(instruction[5])
 
             if instruction.startswith("up "):
-                self.memory["down"] = self.memory["down"] - int(instruction[3])
+                self.memory["down"] -= int(instruction[3])
 
 
             current_line += 1
@@ -38,14 +38,14 @@ class StateMachine2:
 
             
             if instruction.startswith("forward "):
-                self.memory["forward"] = self.memory["forward"] + int(instruction[8])
+                self.memory["forward"] += int(instruction[8])
                 self.memory["depth"] += self.memory["aim"] * int(instruction[8])
 
             if instruction.startswith("down "):
-                self.memory["aim"] = self.memory["aim"] + int(instruction[5])
+                self.memory["aim"] += int(instruction[5])
 
             if instruction.startswith("up "):
-                self.memory["aim"] = self.memory["aim"] - int(instruction[3])
+                self.memory["aim"] -=  int(instruction[3])
 
     
             current_line += 1
